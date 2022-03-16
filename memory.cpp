65a40b33ea54f748a1812eae2053e669ec17aac2
Geovanny Garcia Downing <geogd.712@estudiantec.cr>
2022-03-16 14:26:44 -0600

    First FIles

---
+++ memory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by geogardow on 16/3/22.
+//
+
+#include "memory.h"
---
+++ memory.h
@@ -0,0 +1,14 @@
+//
+// Created by geogardow on 16/3/22.
+//
+
+#ifndef PROYECTO1DATOS2_MEMORY_H
+#define PROYECTO1DATOS2_MEMORY_H
+
+
+class memory {
+
+};
+
+
+#endif //PROYECTO1DATOS2_MEMORY_H
